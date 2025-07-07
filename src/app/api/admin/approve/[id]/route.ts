46ed1b9d7f74cb3d73a0219f152689d5cbe874e3
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2025-07-07 23:10:37 +0530

    deployment fixes

--- src/app/api/admin/approve/[id]/route.ts
+++ src/app/api/admin/approve/[id]/route.ts
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function POST(
-  request: NextRequest,
+  request: Request,
   context: { params: { id: string } }
 ) {
   try {
-    const { id } = context.params;
+  const { id } = context.params;
 
     await prisma.property.update({
       where: { id },
