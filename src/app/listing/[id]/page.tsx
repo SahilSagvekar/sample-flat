beb961800828bddf0c61531e9bb76d1378996fd2
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2025-07-13 12:31:24 +0530

    fixes

--- src/app/listing/[id]/page.tsx
+++ src/app/listing/[id]/page.tsx
@@ -2,14 +2,17 @@ import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import PropertyPageClient from "@/components/property/PropertyPageClient";
 
-interface PropertyPageProps {
-  params: {
-    id: string;
-  };
-}
+// interface PropertyPageProps {
+//   params: {
+//     id: string;
+//   };
+// }
 
-// ✅ async function definition is fine here
-export default async function PropertyPage({ params }: PropertyPageProps) {
+export default async function PropertyPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const property = await prisma.property.findUnique({
     where: { id: params.id },
     include: { seller: true },
@@ -20,11 +23,24 @@ export default async function PropertyPage({ params }: PropertyPageProps) {
   return <PropertyPageClient property={property} />;
 }
 
-// ✅ generateStaticParams function must return an array of params
-export async function generateStaticParams() {
-  const properties = await prisma.property.findMany({ select: { id: true } });
 
-  return properties.map((property) => ({
-    id: property.id.toString(),
-  }));
-}
+// ✅ async function definition is fine here
+// export default async function PropertyPage({ params }: PropertyPageProps) {
+//   const property = await prisma.property.findUnique({
+//     where: { id: params.id },
+//     include: { seller: true },
+//   });
+
+//   if (!property) return notFound();
+
+//   return <PropertyPageClient property={property} />;
+// }
+
+// // ✅ generateStaticParams function must return an array of params
+// export async function generateStaticParams() {
+//   const properties = await prisma.property.findMany({ select: { id: true } });
+
+//   return properties.map((property) => ({
+//     id: property.id.toString(),
+//   }));
+// }
